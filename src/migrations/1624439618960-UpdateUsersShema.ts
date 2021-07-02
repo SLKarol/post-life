8ca4658d016770c5ede2038c8a87f415dc2d4526
Stanislav Karol <slsinitsin@gmail.com>
2021-07-02 23:48:58 +0300

    chore: eslint commands

--- src/migrations/1624439618960-UpdateUsersShema.ts
+++ src/migrations/1624439618960-UpdateUsersShema.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateUsersShema1624439618960 implements MigrationInterface {
-    name = 'UpdateUsersShema1624439618960'
+  name = 'UpdateUsersShema1624439618960';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "bio" character varying NOT NULL DEFAULT ''`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "image" character varying NOT NULL DEFAULT ''`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "bio" character varying NOT NULL DEFAULT ''`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "image" character varying NOT NULL DEFAULT ''`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "bio"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "bio"`);
+  }
 }
--- src/migrations/1624524573172-AddActiveUser.ts
+++ src/migrations/1624524573172-AddActiveUser.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddActiveUser1624524573172 implements MigrationInterface {
-    name = 'AddActiveUser1624524573172'
+  name = 'AddActiveUser1624524573172';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "active" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "active" boolean NOT NULL DEFAULT false`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "active"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "active"`);
+  }
 }
