63d96d329c23e576cc7b4d82834d60f6a1f0d97e
Stanislav Karol <slsinitsin@gmail.com>
2021-07-15 13:06:04 +0300

    feature: add script create function add_article

---
+++ src/migrations/1626341798058-CreateFunctionAddArticle.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateFunctionAddArticle1626341798058
+  implements MigrationInterface
+{
+  name = 'CreateFunctionAddArticle1626341798058';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE OR REPLACE FUNCTION add_article(
+  slug TEXT,
+  title TEXT,
+  description TEXT,
+  body TEXT,
+  author TEXT,
+  tags TEXT []
+) RETURNS json AS $$
+DECLARE id_article uuid = uuid_generate_v4();
+BEGIN
+INSERT INTO articles (id, slug, title, description, body, "authorId")
+VALUES (
+  id_article,
+  slug,
+  title,
+  description,
+  body,
+  author::uuid
+);
+CALL set_tag_for_article(tags, id_article);
+Return (SELECT get_article_json(id_article, author::uuid));
+END;
+$$ LANGUAGE plpgsql;`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop FUNCTION add_article"`);
+  }
+}
