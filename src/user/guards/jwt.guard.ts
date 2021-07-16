53befcd0f894eb92afbc6591bab89e3036584efa
Stanislav Karol <slsinitsin@gmail.com>
2021-07-16 16:03:14 +0300

    feature: написать guard,
    который позволял авторизованным и не авторизованным
    пользователям вызывать метод

--- src/user/guards/jwt.guard.ts
+++ src/user/guards/jwt.guard.ts
@@ -3,3 +3,14 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+/**
+ * Неавторизованные пользователи могут воспользоваться службой.
+ * При этом user будет равен null
+ */
+@Injectable()
+export class AllowNullUserGuard extends AuthGuard('jwt') {
+  handleRequest(err, user) {
+    return user;
+  }
+}
