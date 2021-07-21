f90b97303b0e9f47347d52f5eea3dfd4aae43bcb
Stanislav Karol <slsinitsin@gmail.com>
2021-07-21 19:24:09 +0300

    feature: enable cors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
